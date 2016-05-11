1279085a50217e906cbd6a3e38a9a75c1f7507f2
DonnyLeung <1507568649@qq.com>
2016-05-11 17:45:23 +0800

    基本架构
    js动态计算格子的宽高

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 var window_width = $(window).width();
-	$('.body .box').css({'height':window_width*0.9+'px'})
+	$('.body .box').css({'height':window_width*0.9+'px'});
+
+var box_size = $('.body .box').width();
+var row_size;
+	row_size = box_size/2
+	$('.body .box .row').css({'width': row_size+'px','height':row_size+'px'})
